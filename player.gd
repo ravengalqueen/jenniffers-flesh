fc666460bf6ec8bff0e3f2b95ef604e5639e0310
BunPSX <bunpsx@gmail.com>
2026-04-22 00:10:05 -0300

    silly
    i fixed shit

--- player.gd
+++ player.gd
@@ -1,34 +1,11 @@
-extends RigidBody3D
+extends CharacterBody3D
+var speed : float = 5.0
 
-var mouse_sensitivity := 0.001
-var twist_input := 0.0
-var pitch_input := 0.0
 
-func _ready() -> void:
-	
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-func _process(delta: float) -> void:
-	
-	var input := Vector3.ZERO
-	input.x = Input.get_axis("left_movement", "right_movement")
-	input.z = Input.get_axis("forward_movement", "backward_movement")
-	apply_central_force(basis * input * 1200.00 * delta)
-	
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-	#rotate_y(twist_input) uncommenting will result on mobility loss
-	
-	$Camera3D.rotate_x(pitch_input)
-	$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.5, 1.5)
-	twist_input = 0.0
-	pitch_input = 0.0
-
-func _unhandled_input(event: InputEvent) -> void:
-	
-	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
-			
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	var input_dir : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	velocity.x = input_dir.x * speed
+	velocity.z = input_dir.y * speed
+	move_and_slide()
