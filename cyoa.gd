e01b6bb19c0647a84ce2754a573683b007151d58
brooke <holfrank61@gmail.com>
2026-04-23 21:47:23 +0200

    debug statements removed

--- cyoa.gd
+++ cyoa.gd
@@ -4,10 +4,8 @@ extends Control
 
 func _ready():
 	if cyoa_event:
-		print("_ready called")
 		cyoa_event.set_cyoa_event(self)
 func show_event(index: int):
-	print("show_event called with index: ", index)
 	for child in $HBoxContainer.get_children():
 		child.queue_free()
 		cyoa_event.set_cyoa_event(self)
--- cyoa_events.gd
+++ cyoa_events.gd
@@ -6,8 +6,6 @@ class_name CyoaEvents
 @export var options = Array([], TYPE_STRING, "", null)
 
 func set_cyoa_event(owner_node: Node):
-	print("description: ", description)
-	print("options: ", options)
 	owner_node.get_node("Panel/VBoxContainer/Label").text = description
 	owner_node.get_node("Panel/VBoxContainer/TextureRect").texture = image
 	for option in options:
